0da772b70ba1eb139d66bb896a44aac62cb59ed8
fc58616 <fc58616@alunos.ciencias.ulisboa.pt>
2024-05-19 00:25:14 +0100

    Exercício resolvido

--- src/main/java/com/premiumminds/internship/teknonymy/TeknonymyService.java
+++ src/main/java/com/premiumminds/internship/teknonymy/TeknonymyService.java
@@ -1,16 +1,90 @@
 package com.premiumminds.internship.teknonymy;
 
-import com.premiumminds.internship.teknonymy.Person;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 class TeknonymyService implements ITeknonymyService {
 
-  /**
-   * Method to get a Person Teknonymy Name
-   * 
-   * @param Person person
-   * @return String which is the Teknonymy Name 
-   */
-  public String getTeknonymy(Person person) {
-    throw new RuntimeException("Not Implemented Yet");
-  };
+    /**
+     * Method to get a Person Teknonymy Name
+     *
+     * @param Person person
+     * @return String which is the Teknonymy Name
+     */
+    public String getTeknonymy(Person person) {
+
+        if (hasChildren(person)) {
+            int counter = 1;
+            Pair<Person, Integer> oldest = getOldestTek(person, counter);
+            if (oldest.getSecond() == 1) {
+                String tek = whichSex(person) ? "mother of " : "father of ";
+                return tek + oldest.getFirst().name();
+            } else {
+                StringBuilder builder = new StringBuilder();
+                String tek = whichSex(person) ? "grandmother of " : "grandfather of ";
+                String great = "great-";
+                int finalCounter = oldest.getSecond();
+
+                while (finalCounter > 2){
+                    builder.append(great);
+                    finalCounter--;
+                }
+                return builder.append(tek).append(oldest.getFirst().name()).toString();
+            }
+        }
+
+        return "";
+    }
+
+    private Pair<Person, Integer> getOldestTek(Person person, int counter) {
+        List<Pair<Person, Integer>> oldestTeks = new ArrayList<>();
+        int temp = counter;
+        counter++;
+        if (hasChildren(person)) {
+            Person myOldestChild = null;
+            for (Person child : person.children()) {
+                if(hasChildren(child)) {
+                    Pair<Person, Integer> c = getOldestTek(child, counter);
+                    oldestTeks.add(c);
+                }
+                if (myOldestChild == null) {
+                    myOldestChild = child;
+                } else {
+                   if (ageCompare(child, myOldestChild) == 1) {
+                       myOldestChild = child;
+                   }
+                }
+            }
+            if (myOldestChild != null) {
+                oldestTeks.add(new Pair<>(myOldestChild, temp));
+            }
+        }
+
+        int max = oldestTeks.stream()
+                .mapToInt(Pair::getSecond)
+                .max()
+                .orElse(-1);
+
+        List<Pair<Person, Integer>> furtherAway = oldestTeks.stream()
+                .filter(pair -> pair.getSecond() == max)
+                .toList();
+
+        return furtherAway.stream()
+                .max((pair1, pair2) -> ageCompare(pair1.getFirst(), pair2.getFirst()))
+                .orElse(null);
+    }
+
+    public boolean hasChildren(Person person) {
+        return person.children() != null && person.children().length > 0;
+    }
+
+    public boolean whichSex(Person person) {
+        return person.sex().equals('F');
+    }
+
+    public int ageCompare(Person person1, Person person2) {
+        return person1.dateOfBirth().isBefore(person2.dateOfBirth()) ? 1 : -1;
+    }
 }
--- src/test/java/com/premiumminds/internship/teknonymy/TeknonymyServiceTest.java
+++ src/test/java/com/premiumminds/internship/teknonymy/TeknonymyServiceTest.java
@@ -1,16 +1,7 @@
 package com.premiumminds.internship.teknonymy;
 
-import com.premiumminds.internship.teknonymy.TeknonymyService;
-import com.premiumminds.internship.teknonymy.Person;
-
 import static org.junit.Assert.assertEquals;
-
 import java.time.LocalDateTime;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -19,32 +10,83 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class TeknonymyServiceTest {
 
-  /**
-   * The corresponding implementations to test.
-   *
-   * If you want, you can make others :)
-   *
-   */
-  public TeknonymyServiceTest() {
-  };
+    /**
+     * The corresponding implementations to test.
+     * <p>
+     * If you want, you can make others :)
+     */
+    public TeknonymyServiceTest() {
+    }
 
-  @Test
-  public void PersonNoChildrenTest() {
-    Person person = new Person("John",'M',null, LocalDateTime.of(1046, 1, 1, 0, 0));
-    String result = new TeknonymyService().getTeknonymy(person);
-    String expected = "";
-    assertEquals(result, expected);
-  }
+    ;
 
-  @Test
-  public void PersonOneChildTest() {
-    Person person = new Person(
-        "John",
-        'M',
-        new Person[]{ new Person("Holy",'F', null, LocalDateTime.of(1046, 1, 1, 0, 0)) },
-        LocalDateTime.of(1046, 1, 1, 0, 0));
-    String result = new TeknonymyService().getTeknonymy(person);
-    String expected = "father of Holy";
-    assertEquals(result, expected);
-  }
+    @Test
+    public void PersonNoChildrenTest() {
+        Person person = new Person("John", 'M', null, LocalDateTime.of(1046, 1, 1, 0, 0));
+        String result = new TeknonymyService().getTeknonymy(person);
+        String expected = "";
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void PersonOneChildTest() {
+        Person person = new Person(
+                "John",
+                'M',
+                new Person[]{new Person("Holy", 'F', null, LocalDateTime.of(1046, 1, 1, 0, 0))},
+                LocalDateTime.of(1046, 1, 1, 0, 0));
+        String result = new TeknonymyService().getTeknonymy(person);
+        String expected = "father of Holy";
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void PersonGranFatherOneChildTest() {
+        Person person = new Person("John", 'M',
+                new Person[]{new Person("Holy", 'F',
+                        new Person[]{new Person("Joana", 'F', null, LocalDateTime.of(1046, 1, 1, 0, 0))},
+                        LocalDateTime.of(1046, 1, 1, 0, 0))}, LocalDateTime.of(1999, 5, 1, 1, 0));
+        String result = new TeknonymyService().getTeknonymy(person);
+        String expected = "grandfather of Joana";
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void PersonGreatGranMotherTwoChildTest() {
+        Person person = new Person("A", 'F', new Person[]{
+                new Person("B", 'F', new Person[] {
+                        new Person("C", 'M', new Person[] {
+                                new Person("D", 'M', null, LocalDateTime.of(2023, 1, 1, 0, 0))
+                        }, LocalDateTime.of(2020, 1, 1, 0, 0)),
+                }, LocalDateTime.of(2000, 1, 1, 0, 0)),
+                new Person("B1", 'M', null, LocalDateTime.of(1950, 1, 1, 0, 0))
+        }, LocalDateTime.of(1046, 1, 1, 0, 0));
+
+        String result = new TeknonymyService().getTeknonymy(person);
+        String expected = "great-grandmother of D";
+        assertEquals(expected, result);
+    }
+
+
+    @Test
+    public void PersonGreatGreatGrandFatherTest() {
+        Person a = new Person(
+                "A", 'M',
+                new Person[] {
+                    new Person("B", 'M', new Person[] {
+                            new Person("C", 'M', new Person[] {
+                                    new Person("D", 'M', null, LocalDateTime.of(2000, 1, 1, 0, 0)),
+                                    new Person("E", 'M', new Person[] {
+                                            new Person("F", 'M', null, LocalDateTime.of(2020, 1, 1, 0, 0))
+                                    }, LocalDateTime.of(2001, 1, 1, 0, 0))
+                            }, LocalDateTime.of(1980, 1, 1, 0, 0))
+                    }, LocalDateTime.of(1950, 1, 1, 0, 0))
+                }, LocalDateTime.of(1930, 1, 1, 0, 0)
+        );
+
+      String result = new TeknonymyService().getTeknonymy(a);
+      String expected = "great-great-grandfather of F";
+      assertEquals(expected, result);
+
+    }
 }
